ef1d62f6ac40172f9f94adc566e4f995c03035d0
cpaschall <cpaschall216@gmail.com>
2022-05-04 22:24:48 -0400

    updating index and generateMarkDown js files

--- develop/index.js
+++ develop/index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const md = require('./utils/generateMarkdown.js');
+const fileName = "Readme2.md"
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -35,32 +37,35 @@ const questions = [
         name: "tests"
     },
     {
-        type: 'checkbox',
-        message: 'What license would you like to include in this Readme file?',
-        name: 'license',
-        choices: ['Apache', 'Boost', 'ISC', 'Perl'],
-    },
+        type: "list",
+        message: "Choose a License: ",
+        name: "license",
+        choices: ["Apache", "Boost", "ISC", "Perl"]
+    }
 ];
 
-// Apache: [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-// Boost: [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
-// ISC: [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
-// Perl: [![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)
-
-
-
-
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+function writeToFile(fileName, fileData) {
+    fs.writeFile(fileName, fileData, (err) => {
+        err ? console.error(err) : console.log('Success!');
+    });
+}
 
 // TODO: Create a function to initialize app
 // function init() {}
 
-// Function call to initialize app
-// init();
- 
-inquirer
+function init() {
+    inquirer
     .prompt(questions)
-    .then((response) => {
-        console.log(response.license)
-    })
+    .then(data => {
+        // const license = data.license
+        const fileData = md(data)
+        // console.log(typeof(md.generateMarkdown(data)))
+        // console.log(license)
+        writeToFile(fileName, fileData)
+    });
+}
+
+// Function call to initialize app
+init() 
+// console.log(typeof(md.generateMarkdown(data)))
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,28 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-
+// const license = data.license
 
 function renderLicenseBadge(license) {
-  if (questions.license === ''){
-    return '';
-  } else {
-    return questions.license
-  }
+  // let license = data.license
+  switch (license) {
+      case 'Apache':
+        return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+      case 'Boost':
+        return '![License: Boost](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
+      case 'ISC':
+        return '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)';
+      case 'Perl':
+        return '![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)';
+      default:
+        return '';
+  };
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // let license = data.license
+  switch (license) {
+      case 'Apache':
+        return '[License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+      case 'Boost':
+        return '[License: Boost](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
+      case 'ISC':
+        return '[License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)';
+      case 'Perl':
+        return '[License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)';
+      default:
+        return '';
+  };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseSection(license) {
+  // let license = data.license
+  if(license) {
+    return (`${renderLicenseBadge(license)}<br>${renderLicenseLink(license)}`);
+  } else {
+    return ''
+  };
 }
 
-module.exports = generateMarkdown;
+// TODO: Create a function to generate markdown for README
+
+function generateMarkdown(data) {
+  let license = data.license
+  return (
+  `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Instructions
+  ${data.instructions}
+
+  ## Usage
+  ${data.usage}
+
+  ## How to Contribute
+  ${data.contribution}
+
+  ## Testing
+  ${data.tests}
+
+  ### License
+  ---
+  ${renderLicenseSection(license)}
+  `);
+};
+
+
+module.exports = generateMarkdown
+
